721030e5c9d2cb4bcb029c6dbf00f84f0a767e34
Gleison <gleisonmalheiros@gmail.com>
2023-11-06 00:53:23 +0000

    First commit
    Starting project

---
+++ src/test/java/org/gleison/leetcode/easy/TwoSumTest.java
@@ -0,0 +1,13 @@
+package org.gleison.leetcode.easy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTest {
+
+    @org.junit.jupiter.api.Test
+    void twoSum() {
+
+        
+
+    }
+}
