5dbd609d5bfa3eb6f34abc7d739caa580f1198a5
Gleison <gleisonmalheiros@gmail.com>
2023-11-15 22:33:28 +0000

    Added test for #1

--- src/main/java/org/gleison/leetcode/easy/TwoSum.java
+++ src/main/java/org/gleison/leetcode/easy/TwoSum.java
@@ -3,24 +3,22 @@ package org.gleison.leetcode.easy;
 /**
  * #1
  * https://leetcode.com/problems/two-sum/description/
+ *
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
  */
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = {0,0};
-
-        outer:
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    result[0] = i;
-                    result[1] = j;
-                    break outer;
+                    return new int[]{i, j};
                 }
             }
         }
-
-        return result;
+        return new int[]{};
     }
 
 }
--- src/test/java/org/gleison/leetcode/easy/TwoSumTest.java
+++ src/test/java/org/gleison/leetcode/easy/TwoSumTest.java
@@ -1,13 +1,58 @@
 package org.gleison.leetcode.easy;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@Timeout(value = 10, unit = TimeUnit.SECONDS)
 class TwoSumTest {
 
-    @org.junit.jupiter.api.Test
-    void twoSum() {
+    TwoSum twoSum;
 
-
-
+    @BeforeEach
+    public void setup() {
+        twoSum = new TwoSum();
     }
+
+    @Test
+    void twoSumTest1() {
+        int[] nums = {2, 7, 11, 13};
+        int target = 9;
+
+        int[] actual = twoSum.twoSum(nums, target);
+        int[] expected = {0, 1};
+        Arrays.sort(actual);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void twoSumTest2() {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+
+        int[] actual = twoSum.twoSum(nums, target);
+        int[] expected = {1, 2};
+        Arrays.sort(actual);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void twoSumTest3() {
+        int[] nums = {3, 3};
+        int target = 6;
+
+        int[] actual = twoSum.twoSum(nums, target);
+        int[] expected = {0, 1};
+        Arrays.sort(actual);
+
+        assertArrayEquals(expected, actual);
+    }
+
 }
