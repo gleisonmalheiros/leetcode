4b3398c943515797664a628ad7109df951b82d9d
Gleison <gleisonmalheiros@gmail.com>
2023-11-26 21:40:46 +0000

    commit problem #20

---
+++ src/main/java/org/gleison/leetcode/easy/ValidParentheses.java
@@ -0,0 +1,53 @@
+package org.gleison.leetcode.easy;
+
+import java.util.LinkedList;
+
+/**
+ * #20
+ * https://leetcode.com/problems/valid-parentheses/
+ * <p>
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * An input string is valid if:
+ * - Open brackets must be closed by the same type of brackets.
+ * - Open brackets must be closed in the correct order.
+ * - Every close bracket has a corresponding open bracket of the same type.
+ */
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        var llist = new LinkedList<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if ((s.charAt(i) == '(') ||
+                    (s.charAt(i) == '{') ||
+                    (s.charAt(i) == '[')) {
+                llist.addFirst(s.charAt(i));
+            } else if (llist.isEmpty()) {
+                return false;
+            } else if (s.charAt(i) == ')') {
+                if (llist.getFirst() == '(') {
+                    llist.removeFirst();
+                } else {
+                    return false;
+                }
+            } else if (s.charAt(i) == '}') {
+                if (llist.getFirst() == '{') {
+                    llist.removeFirst();
+                } else {
+                    return false;
+                }
+            } else if (s.charAt(i) == ']') {
+                if (llist.getFirst() == '[') {
+                    llist.removeFirst();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return llist.isEmpty();
+    }
+
+}
---
+++ src/test/java/org/gleison/leetcode/easy/ValidParenthesesTest.java
@@ -0,0 +1,57 @@
+package org.gleison.leetcode.easy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Timeout(value = 10, unit = TimeUnit.SECONDS)
+class ValidParenthesesTest {
+
+    ValidParentheses valPar;
+
+    @BeforeEach
+    void setUp() {
+        valPar = new ValidParentheses();
+    }
+
+    @Test
+    void isValid1() {
+        var actual = valPar.isValid("()");
+        var expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void isValid2() {
+        var actual = valPar.isValid("()[]{}");
+        var expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void isValid3() {
+        var actual = valPar.isValid("[(()[]{})]");
+        var expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void isValid4() {
+        var actual = valPar.isValid("(]");
+        var expected = false;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void isValid5() {
+        var actual = valPar.isValid("]");
+        var expected = false;
+        assertEquals(expected, actual);
+    }
+
+}
